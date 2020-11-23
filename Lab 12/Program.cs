1b019453788cb995de3ffaa4cb7d4da5d837d17b
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-11-23 14:23:46 -0800

    Add project files.

---
+++ Lab 12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_12
+{
+    using System;
+
+    class GFG
+    {
+
+
+        static public void my_add(string str1, string str2,
+                             string str3 = "GeeksforGeeks")
+        {
+            Console.WriteLine(str1 + str2 + str3);
+        }
+
+        static public void Main()
+        {
+            my_add("Welcome", "to");
+            my_add("This", "is", "C# Tutorial");
+        }
+    }
+}
